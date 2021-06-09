ae26bd045635caeff9dfb8397027f936a0289d6e
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-09 11:41:13 +0300

    Main.html Created

--- rally/views.py
+++ rally/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render , redirect
+import datetime as dt
 # Create your views here.
+
+def main(request):
+    return render(request, 'main.html')
+
